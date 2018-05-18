f86f524b62caf23129b4fa52154f13969f72d615
clairose <clairose@umich.edu>
2018-05-18 14:15:28 -0400

    Added ng-if and ng-class

--- controller.js
+++ controller.js
@@ -4,23 +4,37 @@
     function TodoController() {
         let task1 = {
             task: "Walk the dog.",
-            completed: false
+            className: ""
         }
         let task2 = {
             task: "Go to the store.",
-            completed: true
+            className: "complete"
         }
         let task3 = {
             task: "Get gas.",
-            completed: false
+            className: ""
         }
         let task4 = {
             task: "Wash the car.",
-            completed: false
+            className: ""
         }
         const vm = this;
         vm.todolist = [task1, task2, task3, task4];
     }
+
+        var fetch = angular.module('todoapp', []);
+
+        fetch.controller('secondController', ['$scope', '$http', function ($scope, $http) {
+          $scope.class1 = false;
+          $scope.clicked = true;
+
+          $scope.changeStyle = function(){
+            $scope.class1 = true;
+            $scope.clicked = false;
+          }
+        }])
+
+    
     angular
         .module("todoapp")
         .controller("TodoController", TodoController);
